e1f5bbf14389e8cff4c87ca99fe579ff4b2a00dd
joaovts14 <45018299+joaovts14@users.noreply.github.com>
2026-07-07 21:53:11 -0300

    use state

--- app/jogos/page.tsx
+++ app/jogos/page.tsx
@@ -38,7 +38,7 @@ export default function Jogos() {
   const [user, setUser] = useState<any>(null);
   const [palpites, setPalpites] = useState<Record<number, Palpite>>({});
 
-  const [faseSelecionada, setFaseSelecionada] = useState("round_of_16");
+  const [faseSelecionada, setFaseSelecionada] = useState("quarterfinals");
   const [rodadaSelecionada, setRodadaSelecionada] = useState(1);
 
   const [loading, setLoading] = useState(true);
